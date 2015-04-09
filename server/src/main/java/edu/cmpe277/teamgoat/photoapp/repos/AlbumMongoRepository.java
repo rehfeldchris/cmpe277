bac767544094b0e6fb1378c01c8dc0545b6e94c3
Sai Karra <srkarra@us.ibm.com>
2015-04-09 14:00:58 -0700

    Sample spring data mongo repo

--- server/src/main/java/edu/cmpe277/teamgoat/photoapp/repos/AlbumMongoRepository.java
+++ server/src/main/java/edu/cmpe277/teamgoat/photoapp/repos/AlbumMongoRepository.java
@@ -5,7 +5,12 @@ import edu.cmpe277.teamgoat.photoapp.dto.Album;
 import edu.cmpe277.teamgoat.photoapp.dto.Image;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface AlbumMongoRepository extends MongoRepository<Album, String> {
 
+
+    List<Album> findByOwnerId(String ownerId);
+    List<Album> findByOwnerIdIgnoreCase(String ownerId);
     
 }
