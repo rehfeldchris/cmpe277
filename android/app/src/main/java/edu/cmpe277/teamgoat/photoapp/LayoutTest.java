122ce1144e5c863e31303622338ce54de5bbcd8f
Thong Nguyen <nhthong2007@gmail.com>
2015-04-25 15:42:42 -0700

    Add: 	- layout_drawerlayout_sidemenu.xml: DrawerLayout for sidebar menu 	- Album Item changes 	- layout_fragment_friendlist_item.xml: Layout for Friend list item 	- layout_gridlayout_view_picture.xml: Layout for display picture

--- android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/LayoutTest.java
+++ android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/LayoutTest.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 /**
  * Created by squall on 4/16/15.
  */
-public class Profile extends Activity
+public class LayoutTest extends Activity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_container_album_item);
+//        setContentView(R.layout.layout_fragment_container_album_item);
+        setContentView(R.layout.layout_gridlayout_view_picture);
     }
+
 }
