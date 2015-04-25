c64818f2cfd698155ad677f9bfec95ea1d90fe2b
chris rehfeld <rehfeldchris@gmail.com>
2015-04-25 14:55:39 -0700

    add accessors to user entity

--- server/src/main/java/edu/cmpe277/teamgoat/photoapp/dto/User.java
+++ server/src/main/java/edu/cmpe277/teamgoat/photoapp/dto/User.java
@@ -15,5 +15,45 @@ public class User {
 
     private Date whenDataFetchedFromFacebook;
 
+    public User() {
+    }
 
+    public User(String facebookUserId, String name, String profilePhotoUrl, Date whenDataFetchedFromFacebook) {
+        this.facebookUserId = facebookUserId;
+        this.name = name;
+        this.profilePhotoUrl = profilePhotoUrl;
+        this.whenDataFetchedFromFacebook = whenDataFetchedFromFacebook;
+    }
+
+    public String getFacebookUserId() {
+        return facebookUserId;
+    }
+
+    public void setFacebookUserId(String facebookUserId) {
+        this.facebookUserId = facebookUserId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfilePhotoUrl() {
+        return profilePhotoUrl;
+    }
+
+    public void setProfilePhotoUrl(String profilePhotoUrl) {
+        this.profilePhotoUrl = profilePhotoUrl;
+    }
+
+    public Date getWhenDataFetchedFromFacebook() {
+        return whenDataFetchedFromFacebook;
+    }
+
+    public void setWhenDataFetchedFromFacebook(Date whenDataFetchedFromFacebook) {
+        this.whenDataFetchedFromFacebook = whenDataFetchedFromFacebook;
+    }
 }
