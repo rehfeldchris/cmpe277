d5f1f9af4d08c3f7ecc016ad713ec98e27052e8f
Sai Karra <srkarra@us.ibm.com>
2015-04-25 19:03:23 -0700

    Added logger and IDs

---
+++ android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/util/IDs.java
@@ -0,0 +1,10 @@
+package edu.cmpe277.teamgoat.photoapp.util;
+
+/**
+ * Created by srkarra on 4/25/2015.
+ */
+public class IDs {
+
+    public static final String BASE_PACKAGE_NAME        = "edu.cmpe277.teamgoat.photoapp";
+
+}
---
+++ android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/util/PhotoAppLog.java
@@ -0,0 +1,53 @@
+package edu.cmpe277.teamgoat.photoapp.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by srkarra on 4/25/2015.
+ */
+public class PhotoAppLog {
+
+    private Logger log;
+
+    public PhotoAppLog(Logger log) {
+        this.log = log;
+    }
+
+    public void info(String msg) {
+        log(Level.INFO, msg);
+    }
+
+    public void error(String msg) {
+        log(Level.SEVERE, msg);
+    }
+
+    public void error(Exception e) {
+        log(Level.SEVERE, getStackTrace(e));
+    }
+
+    public void error (String msg, Exception e) {
+        error(msg);
+        error(e);
+    }
+
+    public void log(Level logLevel, String msg) {
+        log.log(logLevel,msg);
+    }
+
+
+    /**
+     * Returns the stack trace for any exception
+     *
+     * @param e Exception with a stack trace
+     * @return String containing the stack trace
+     */
+    public static String getStackTrace(Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw, true));
+        return sw.getBuffer().toString();
+    }
+
+}
