d21e966f53d7d942d17f5f889d6c696a805776a1
Sai Karra <srkarra@us.ibm.com>
2015-04-25 20:30:16 -0700

    Added additional Facebook permission

--- android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/util/IDs.java
+++ android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/util/IDs.java
@@ -1,10 +1,19 @@
 package edu.cmpe277.teamgoat.photoapp.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by srkarra on 4/25/2015.
  */
 public class IDs {
 
-    public static final String BASE_PACKAGE_NAME        = "edu.cmpe277.teamgoat.photoapp";
+    /** PhotoApp Base Package for Logging and Preferences */
+    public static final String BASE_PACKAGE_NAME                    = "edu.cmpe277.teamgoat.photoapp";
 
+
+    /** Required permission for Facebook */
+    public static final String[] FACEBOOK_LOGIN_PERMISSIONS         = {
+        "user_about_me", "user_friends", "read_custom_friendlists", "email"
+    };
 }
