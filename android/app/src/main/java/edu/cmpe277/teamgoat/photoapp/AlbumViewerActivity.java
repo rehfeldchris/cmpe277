5d9436df395b6482dced0556616059cd3239e327
caritaou <carita.ou@gmail.com>
2015-05-07 16:30:53 -0700

    add upload button

--- android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/AlbumViewerActivity.java
+++ android/app/src/main/java/edu/cmpe277/teamgoat/photoapp/AlbumViewerActivity.java
@@ -1,14 +1,26 @@
 package edu.cmpe277.teamgoat.photoapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import edu.cmpe277.teamgoat.photoapp.model.Album;
+import edu.cmpe277.teamgoat.photoapp.model.Image;
 
 
 public class AlbumViewerActivity extends ActionBarActivity
 {
+    private static int RESULT_LOAD_IMG = 1;
+    String imagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +52,46 @@ public class AlbumViewerActivity extends ActionBarActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
 
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imagePath = cursor.getString(columnIndex);
+                cursor.close();
+
+                Toast.makeText(this, imagePath, Toast.LENGTH_LONG)
+                        .show();
+
+//                Album currentAlbum = PhotoAlbums.albumUserMostRecentlyClicked;
+//                currentAlbum.getImages().add(new Image());
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+        }
+
+    }
 }
