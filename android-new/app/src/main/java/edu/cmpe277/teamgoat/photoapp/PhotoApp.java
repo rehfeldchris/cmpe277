b604cfcb8ab928541011a456bd4a7dfd68760539
Sai Karra <srkarra@us.ibm.com>
2015-05-10 10:33:58 -0700

    Beable to pull api broker from app

--- android-new/app/src/main/java/edu/cmpe277/teamgoat/photoapp/PhotoApp.java
+++ android-new/app/src/main/java/edu/cmpe277/teamgoat/photoapp/PhotoApp.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 
 import java.util.logging.Logger;
 
+import edu.cmpe277.teamgoat.photoapp.model.ApiBroker;
 import edu.cmpe277.teamgoat.photoapp.util.IDs;
 
 public class PhotoApp extends Application {
@@ -12,6 +13,8 @@ public class PhotoApp extends Application {
     private SharedPreferences masterPreferences;
 
     private String facebookAccessToken;
+
+    private ApiBroker apiBroker;
 
     public SharedPreferences getMasterPreferences() {
         if (masterPreferences == null) {
@@ -31,5 +34,25 @@ public class PhotoApp extends Application {
 
     public void setFacebookAccessToken(String facebookAccessToken) {
         this.facebookAccessToken = facebookAccessToken;
+    }
+
+    public String getServerUrl() {
+        return getServerUrl(true);
+    }
+
+    public String getServerUrl(boolean production) {
+        return production ? "https://srkarra.com:444" : "http://10.0.2.2:80";
+    }
+
+    public void forceRecreateApiInstance() {
+        apiBroker = new ApiBroker(this);
+    }
+
+    public ApiBroker getApiBroker() {
+        if(apiBroker == null) {
+            forceRecreateApiInstance();
+        }
+
+        return apiBroker;
     }
 }
